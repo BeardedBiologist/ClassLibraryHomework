4627e2990de9b08b5851d2ad484921d84fe63754
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-04-30 14:49:44 +0200

    init commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PersonLibrary;
 
+namespace PersonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Doe";
+            person.Age = 30;
+
+            person.SayHello();
+
+            Console.ReadKey();
+        }
+    }
+}
